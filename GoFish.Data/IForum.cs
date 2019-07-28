40fb3a5572a2fbbf01caf7f1373f7876d56f7307
SpearInTheWild <mmspear@gmail.com>
2019-07-28 17:05:51 -0400

    Initiate Service Layer

---
+++ GoFish.Data/IForum.cs
@@ -0,0 +1,24 @@
+﻿using GoFish.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<IdentityUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int id);
+        Task UpdateForum(Forum forum);
+
+
+
+
+    }
+}
---
+++ GoFish.Service/ForumService.cs
@@ -0,0 +1,49 @@
+﻿using GoFish.Data;
+using GoFish.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GoFish.Service
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _context;
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums.Include(f => f.Posts);
+        }
+
+        public IEnumerable<IdentityUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForum(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
