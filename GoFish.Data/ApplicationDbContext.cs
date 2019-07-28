0a4b12e408ce3ba47f6d4b014661b4294ada0c05
SpearInTheWild <mmspear@gmail.com>
2019-07-28 15:10:13 -0400

    Initial Commit

---
+++ GoFish.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GoFish.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoFish.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+    }
+}
---
+++ GoFish.Data/Models/PostReply.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoFish.Data.Models
+{
+    public class PostReply
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual IdentityUser User { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
